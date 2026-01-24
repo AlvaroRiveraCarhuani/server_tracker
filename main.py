90997c7fa2331ac06216cb540b51db9b4d1f1fb6
Alvaro <alvarorivera4513@gmail.com>
2026-01-24 18:39:09 -0400

    modification and correct errors

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routers import server 
+from database import Base, engine
 import models
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def hello():
