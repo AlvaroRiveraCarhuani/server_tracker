93aaa6456cad8707d7a5d6ec44ea94420bdf3c48
Alvaro <alvarorivera4513@gmail.com>
2026-02-08 18:42:19 -0400

    add: new endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import server 
+from routers import server,target
 from database import Base, engine
 import models
 
@@ -8,7 +8,7 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
         
 app.include_router(server.router)
-
+app.include_router(target.router)
 
 @app.get("/")
 def hello():
