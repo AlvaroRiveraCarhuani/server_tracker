ba8fcf56502c0fcb63d035c518dc375f0e3e81af
Alvaro Rivera <alvaro.regulo.rivera@gmail.com>
2026-02-19 22:35:24 -0400

    add: new functionality for update target

--- routers/target.py
+++ routers/target.py
@@ -4,7 +4,7 @@ from sqlalchemy.exc import IntegrityError
 from typing import List
 from database import get_db
 from models.target import Target
-from schemas.target_schema import TargetCreate, TargetResponse
+from schemas.target_schema import TargetCreate, TargetResponse, TargetUpdate
 from auth.authApiKey import get_api_key 
 
 router = APIRouter(prefix="/targets", tags=["Targets Configuration"])
@@ -36,3 +36,23 @@ def delete_target(id: int, db: Session = Depends(get_db)):
         db.commit()
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error to delete target, {e}")
+
+router.patch("/", dependencies=[Depends(get_api_key)])
+def update_target(id: int, target_data: TargetUpdate, db: Session= Depends(get_db)):
+    target = db.get(Target, id)
+
+    if not target:
+        raise HTTPException(status_code=404, detail=f"Target {id} not found")
+    update_dict = target_data.model_dump(exclude_unset=True)
+    
+    if not update_dict:
+        raise HTTPException(status_code=400, detail=f"No fields provided for update")
+    try:
+        for key, value in update_dict.items():
+            setattr(target,key,value)
+        db.commit()
+        db.refresh(target)
+        return {"message": f"Successful update", "data": target}
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f"Error to update, {id}")
